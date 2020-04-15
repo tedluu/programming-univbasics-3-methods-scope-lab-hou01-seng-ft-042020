adc22b3132d36ed82f8ea6cd9847135dbb510ccd
Thach Luu <luuphanbaothach@gmail.com>
2020-04-15 09:11:56 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,9 @@
+ 
+phrase = "It's-a me, Mario!"
+ 
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
