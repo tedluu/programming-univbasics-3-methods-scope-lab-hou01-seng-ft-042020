ecba77c91825ec04eef64e2bfa1a5148bc58cffa
Thach Luu <luuphanbaothach@gmail.com>
2020-04-15 09:12:25 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,12 +1,14 @@
  
-phrase = "It's-a me, Mario!"
  
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   
 def mario
+  phrase = "It's-a me, Mario!"
+
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
   puts status
 end
